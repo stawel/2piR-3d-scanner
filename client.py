be4ea2b667e33b7b8ec5ba2c60254ad75bc68554
Paweł Stawicki <stawel+git@gmail.com>
2016-10-13 21:52:04 +0000

    laser

--- client.py
+++ client.py
@@ -7,12 +7,23 @@ from pi2R.hardware import *
 
 
 stepper_init()
+laser_init()
+
 #stepper_goto(12)
 
-stepper_goto(1,-1)
+laser(1)
+stepper_goto(300,1)
+laser(0)
+stepper_goto(600,1)
+laser(1)
+stepper_goto(0,-1)
+laser_off()
+
 
 #stepper_goto(0,-1)
 
 #stepper_goto(+1200,-1)
 
 #stepper_goto(0)
+
+cleanup()
--- pi2R/hardware.py
+++ pi2R/hardware.py
@@ -2,6 +2,9 @@ import time
 import RPi.GPIO as GPIO
 import sys
 
+laser1_pin = 18
+
+
 stepper_pin_A = 2
 stepper_pin_B = 3
 stepper_pin_C = 4
@@ -12,13 +15,17 @@ stepper_full_rotation=4096/2*3
 
 stepper_steps = [8+1, 4+8, 2+4, 1+2]
 
+
 def stepper_init():
     global stepper_position
     gpio_list = [stepper_pin_A,stepper_pin_B,stepper_pin_C,stepper_pin_D]
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(gpio_list, GPIO.OUT)
     stepper_position = 0
+    stepper_off()
 
+def cleanup():
+    GPIO.cleanup()
 
 def _set_step(x):
 #    print 'x:' ,x
@@ -45,3 +52,19 @@ def stepper_goto(pos, direction = 1, delay = 0.003, turn_off = True):
     if turn_off:
         stepper_off()
 
+
+
+
+
+def laser_init():
+    gpio_list = [laser1_pin]
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(gpio_list, GPIO.OUT)
+    laser_off()
+
+def laser(nr):
+    GPIO.output(laser1_pin, nr&1)
+
+
+def laser_off():
+    laser(0)
