96b5179dc636479015d8ca7b55fdf83eb6a90a97
Paweł Stawicki <stawel+git@gmail.com>
2016-10-13 21:40:57 +0000

    stepper

--- client.py
+++ client.py
@@ -7,4 +7,12 @@ from pi2R.hardware import *
 
 
 stepper_init()
-stepper_goto(12)
+#stepper_goto(12)
+
+stepper_goto(1,-1)
+
+#stepper_goto(0,-1)
+
+#stepper_goto(+1200,-1)
+
+#stepper_goto(0)
--- pi2R/hardware.py
+++ pi2R/hardware.py
@@ -8,11 +8,12 @@ stepper_pin_C = 4
 stepper_pin_D = 14
 
 stepper_position = 0
-stepper_full_rotation=4096*3
+stepper_full_rotation=4096/2*3
 
-stepper_steps = [1+2, 2+4, 4+8, 8+1]
+stepper_steps = [8+1, 4+8, 2+4, 1+2]
 
 def stepper_init():
+    global stepper_position
     gpio_list = [stepper_pin_A,stepper_pin_B,stepper_pin_C,stepper_pin_D]
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(gpio_list, GPIO.OUT)
@@ -21,10 +22,10 @@ def stepper_init():
 
 def _set_step(x):
 #    print 'x:' ,x
-    GPIO.output(A, x&1)
-    GPIO.output(B, x&2)
-    GPIO.output(C, x&4)
-    GPIO.output(D, x&8)
+    GPIO.output(stepper_pin_A, x&1)
+    GPIO.output(stepper_pin_B, x&2)
+    GPIO.output(stepper_pin_C, x&4)
+    GPIO.output(stepper_pin_D, x&8)
 
 def _make_step(x):
     x = x % 4;
@@ -33,7 +34,8 @@ def _make_step(x):
 def stepper_off():
     _set_step(0)
 
-def stepper_goto(pos, direction = 1, delay = 0.03, turn_off = True):
+def stepper_goto(pos, direction = 1, delay = 0.003, turn_off = True):
+    global stepper_position, stepper_full_rotation
     pos %= stepper_full_rotation
     while pos != stepper_position:
         stepper_position += direction
