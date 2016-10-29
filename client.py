bdee5e31ef99c2f9569ff619f2339a94049d1990
Paweł Stawicki <stawel+git@gmail.com>
2016-10-29 22:40:31 +0000

    network thread

--- client.py
+++ client.py
@@ -5,28 +5,51 @@ import struct
 import time
 import picamera
 import RPi.GPIO as GPIO
-from fractions import Fraction
+import cv2
+import numpy as np
+import zlib
+from numpy.lib.stride_tricks import as_strided
 
+from fractions import Fraction
 from pi2R.hardware import *
+import threading
+import Queue
 
 
 img_type = 2
 img_type = 1 #bayer
 
+sensor_version = 1
+
 stepper_init()
 laser_init()
-
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
 client_socket.connect(('192.168.2.214', 8000))
+#client_socket.setblocking(0)
+# Make a file-like object out of the connection
+connection = client_socket.makefile('wb')#, 20*1000*1000)
 
+network_queue = Queue.Queue()
+
+def network_thread():
+    while True:
+        r = network_queue.get()
+        length = len(r)
+        connection.write(struct.pack('<L', img_type))
+        connection.write(struct.pack('<L', length))
+        t3 = time.time()
+        connection.write(r)
+        network_queue.task_done()
+
+worker = threading.Thread(target=network_thread)
+worker.setDaemon(True)
+worker.start()
 
 state = True
 pos = 0
-# Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
 
 def camera_info(camera):
     print 'awb_gains:', camera.awb_gains
@@ -38,6 +61,57 @@ def camera_info(camera):
     print 'analog_gain:', camera.analog_gain
     print 'sharpness:', camera.sharpness
 
+def get_bayer_offset():
+    offset = {
+        1: 6404096,
+        2: 10270208,
+        }[sensor_version]
+    return offset
+
+def image_bayer_small(stream):
+    offset = get_bayer_offset()
+    data = stream.getvalue()[-offset:]
+    assert data[:4] == 'BRCM'
+    data = data[32768:]
+    data = np.fromstring(data, dtype=np.uint8)
+
+    reshape, crop = {
+        1: ((1952, 3264), (1944, 3240)),
+        2: ((2480, 4128), (2464, 4100)),
+        }[sensor_version]
+    data = data.reshape(reshape)[:crop[0], :crop[1]]
+
+    data = data.astype(np.uint16) << 2
+#    print 'bayer1:',data[1000,1000:1020]
+    for byte in range(4):
+        data[:, byte::5] |= ((data[:, 4::5] >> ((4 - byte) * 2)) & 0b11)
+    data = np.delete(data, np.s_[4::5], 1)
+
+#    print 'bayer2:',data[1000,1000:1020]
+    (y,x) = data.shape
+    rgb = np.zeros((y/2,x/2,3), dtype=data.dtype)
+    rgb[:, :, 0] = data[0::2, 1::2]*2 # Blue
+    rgb[:, :, 1] = data[0::2, 0::2]   # Green
+    rgb[:, :, 1] += data[1::2, 1::2]  # Green
+    rgb[:, :, 2] = data[1::2, 0::2]*2 # Red
+
+    return rgb<<5
+
+def make_img(data_stream, stream):
+    t1 = time.time()
+    img = image_bayer_small(data_stream)
+    t2 = time.time()
+    r, buf = cv2.imencode('.png', img)
+    t3 = time.time()
+    data_stream.seek(0)
+    data_stream.truncate()
+    stream.write(bytearray(buf))
+    t4 = time.time()
+    print 'make img time:', t2-t1,t3-t2,t4-t3
+    return stream
+
+def make_img2(data_stream, stream):
+    return data_stream
 
 try:
     camera = picamera.PiCamera(resolution=(2592,1944), framerate=Fraction(5, 1))
@@ -54,7 +128,7 @@ try:
 
 #    camera.iso = 800
 
-    time.sleep(10)
+    time.sleep(5)
     
     camera.shutter_speed = camera.exposure_speed
     camera.exposure_mode = 'off'
@@ -73,6 +147,7 @@ try:
     # our protocol simple)
     start = time.time()
     stream = io.BytesIO()
+    data_stream = io.BytesIO()
 
     if img_type == 1:
         img_bayer = True
@@ -80,34 +155,48 @@ try:
         img_bayer = False
 
     for foo in range(1,20000):
-        camera.capture(stream, 'jpeg', bayer=img_bayer)
+        t1 = time.time()
+        camera.capture(data_stream, 'jpeg', bayer=img_bayer)
         # Write the length of the capture to the stream and flush to
         # ensure it actually gets sent
+        t2 = time.time()
+        stream = make_img2(data_stream, stream)
+        t3 = time.time()
 
         camera_info(camera)
 
         length = stream.tell()
-        connection.write(struct.pack('<L', img_type))
-        connection.write(struct.pack('<L', length))
-        connection.flush()
+        if img_type == 1:
+            offset = length - get_bayer_offset()
+        else:
+            offset = 0
+
+#        connection.flush()
         # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
+        stream.seek(offset)
+        r = stream.read()
+        network_queue.put(r)
+        t4 = time.time()
+
         # If we've been capturing for more than 30 seconds, quit
 #        if time.time() - start > 300:
 #            break
         # Reset the stream for the next capture
         stream.seek(0)
         stream.truncate()
-        print time.time(), length
+        print 'current time:', time.time(), 'qsize:', network_queue.qsize()
         state = not state
         if state:
             pos+=1
             stepper_goto(pos,1, delay=0.7)
             laser(0)
+            while network_queue.qsize() > 3:
+                print 'waiting queue:', network_queue.qsize()
             time.sleep(0.3)
         else:
             laser(1)
+        t5 = time.time()
+        print 'time total:', t5-t1, ' pt:', t2-t1,t3-t2,t4-t3,t5-t4
 
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', img_type))
